cd9d1ee10e7a30a6558daa7098de9e59c5fc090a
Leonard <leonardbinet@gmail.com>
2016-12-16 11:12:37 +0100

    Add raw test, for simple request and with extra parameters

---
+++ test/test_raw.py
@@ -0,0 +1,44 @@
+"""
+Tests for raw request module.
+
+"""
+
+import responses
+import time
+
+import navitia_client
+import test as _test
+import requests
+
+
+class RawTest(_test.TestCase):
+
+    def setUp(self):
+        self.user = 'leo'
+        self.core_url = "https://api.sncf.com/v1/"
+        self.client = navitia_client.Client(self.user)
+
+    @responses.activate
+    def test_simple_request(self):
+        responses.add(responses.GET,
+                      "https://api.sncf.com/v1/coverage/sncf/disruptions",
+                      body='yeah',
+                      status=200,
+                      content_type="application/json")
+
+        self.client.raw("coverage/sncf/disruptions")
+        self.assertEqual(1, len(responses.calls))
+
+    # could test retriable status, and parameters
+    @responses.activate
+    def test_extra_params(self):
+        responses.add(responses.GET,
+                      "https://api.sncf.com/v1/coverage/sncf/disruptions",
+                      body='{"status":"OK","results":[]}',
+                      status=200,
+                      content_type="application/json")
+        self.client.raw("coverage/sncf/disruptions",
+                        extra_params={"foo": "bar", "yo": "ya"})
+        self.assertEqual(1, len(responses.calls))
+        self.assertURLEqual("https://api.sncf.com/v1/coverage/sncf/disruptions?foo=bar&yo=ya",
+                            responses.calls[0].request.url)
--- test_runner.py
+++ test_runner.py
@@ -5,7 +5,7 @@ import unittest
 # load package to initialize
 import test
 # import test modules
-from test import test_client, test_explore, test_inverted_geocoding, test_journeys, test_route_schedules
+from test import test_client, test_explore, test_inverted_geocoding, test_journeys, test_route_schedules, test_raw
 
 # initialize the test suite
 loader = unittest.TestLoader()
@@ -17,6 +17,7 @@ suite.addTests(loader.loadTestsFromModule(test_explore))
 suite.addTests(loader.loadTestsFromModule(test_inverted_geocoding))
 suite.addTests(loader.loadTestsFromModule(test_journeys))
 suite.addTests(loader.loadTestsFromModule(test_route_schedules))
+suite.addTests(loader.loadTestsFromModule(test_raw))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
